51eb17fc88c16656bbe938769273a37c72599ab4
sean-mahan <seanmahansean@gmail.com>
2022-06-30 19:30:18 -0500

    added styling for navigation bar, along with some general optimizations and organization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {Routes, Route} from "react-router-dom"
 
 import Home from "./routes/home/index"
-import Navigation from "./components/navigation-bar/index"
+import Navigation from "./routes/navigation/index"
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   )
 }
