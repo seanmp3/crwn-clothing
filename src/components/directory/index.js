c6a0980b9e72eca3708ec6b649529e35d0eec1de
sean-mahan <seanmahansean@gmail.com>
2022-08-04 14:59:00 -0500

    fixed some styling clashing, installed styled-components

--- src/components/directory/index.js
+++ src/components/directory/index.js
@@ -1,4 +1,4 @@
-import CategoryItem from "../categoryItem/index"
+import DirectoryItem from "../directoryItem/index"
 
 import "../../styles/directory.scss"
 
@@ -6,7 +6,7 @@ const Directory = ({categories}) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
   )
